e622b65979bf3edec4049fb43e6eabe3c90bd341
Stefan Pop <stefan9161@gmail.com>
2021-06-30 20:23:37 +0200

    updated the App component to a class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-		
-		</div>
-	);
+
+class App extends Component {
+	
+	render() {
+
+		return (
+			<div className="App">
+				<CitySearch />
+				<EventList />
+				<NumberOfEvents />
+			</div>
+		);
+	}
 }
 
 export default App;
