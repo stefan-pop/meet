78b6ff53391a8890599f1f5613ebc51047455abe
Stefan Pop <stefan9161@gmail.com>
2021-07-04 16:22:58 +0200

    fixed a typo

--- src/api.js
+++ src/api.js
@@ -57,7 +57,7 @@ export const getEvents = async () => {
         const result = await axios.get(url);
         if (result.data) {
             var locations = extractLocations(result.data.events);
-            localStorage.setItem("lastEvents", JSON.stringiy(result.data));
+            localStorage.setItem("lastEvents", JSON.stringify(result.data));
             localStorage.setItem("locations", JSON.stringify(locations));
         }
         NProgress.done();
