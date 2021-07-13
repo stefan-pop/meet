1ae5f58b8a123387a412858f9872d97afc58d59f
Stefan Pop <stefan9161@gmail.com>
2021-07-13 19:16:52 +0200

    created an Alert component and 2 other components that inherit the base component(Alert)

---
+++ src/Alert.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Alert extends Component {
+    constructor(props) {
+        super(props);
+        this.color = null;
+    }
+
+    getStyle = () => {
+        return {
+            color: this.color,
+        };
+    }
+
+    render() {
+        return (
+            <div className="Alert">
+                <small style={this.getStyle()}>{this.props.text}</small>
+            </div>
+        )
+    }
+}
+
+class InfoAlert extends Alert {
+    constructor(props) {
+        super(props);
+        this.color = 'blue';
+    }
+}
+
+class ErrorAlert extends Alert {
+    constructor(props) {
+        super(props);
+        this.color = 'red';
+    }
+}
+
+export {InfoAlert, ErrorAlert};
+
+Alert.propTypes = {
+    text: PropTypes.string.isRequired
+}
